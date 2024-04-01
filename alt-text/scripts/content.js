7ceabf3f6b3cd25702003f3d9ec9e633d7272826
Keshav Chawla <hello@keshavchawla.com>
2024-03-31 23:42:03 -0400

    feature: add custom prompts for toggle features

--- alt-text/scripts/content.js
+++ alt-text/scripts/content.js
@@ -1,18 +1,23 @@
 function openai_get_alt_text(image_url, apiKey, includeColorDescription, extraDetailedDescription) {
   const openai_endpoint = "https://api.openai.com/v1/chat/completions";
   const openai_model = "gpt-4-vision-preview";
+
+  const baseline_role = "You are an assistant to help provide alt-text descriptions for images on websites missing these alt-text attributes. Generate and return back just an alt-text description for this image. Don't preface your answer with alt-text or any other indicator. Make sure that alt-text you provide follows the recommended guidelines for alt-text on the web."
+  const color_role = "You are an assistant to help provide alt-text descriptions for images on websites missing these alt-text attributes. Generate and return back just an alt-text description for this image. Don't preface your answer with alt-text or any other indicator. Make sure that alt-text you provide follows the recommended guidelines for alt-text on the web. You have a specific intent to also describe the colours in the image so be sure to include that in your response as well."
+  const baseline_prompt = "Generate an alt-text description for this image. Keep it brief."
+  const descriptive_prompt = "Generate an alt-text description for this image. Be descriptive in your response."
+
   messages = [
     {
       role: "system",
-      content:
-        "You are an assistant to help provide alt-text descriptions for images on websites missing these alt-text attributes. Generate and return back just an alt-text description for this image. Don't preface your answer with alt-text or any other indicator. Make sure that alt-text you provide follows the recommended guidelines for alt-text on the web.",
+      content: includeColorDescription ? color_role : baseline_role,
     },
     {
       role: "user",
       content: [
         {
           type: "text",
-          text: "Generate an alt-text description for this image.",
+          text: extraDetailedDescription ? descriptive_prompt : baseline_prompt,
         },
         {
           type: "image_url",
@@ -48,15 +53,15 @@ $(document).ready(async function () {
   });
   
   await chrome.storage.local.get(["overwriteAltTags"]).then((result) => {
-    OVERWRITE_ALT_TAGS = result.apiKey;
+    OVERWRITE_ALT_TAGS = result.overwriteAltTags;
   });
   
   await chrome.storage.local.get(["includeColorDescription"]).then((result) => {
-    INCLUDE_COLOR_DESCRIPTION = result.apiKey;
+    INCLUDE_COLOR_DESCRIPTION = result.includeColorDescription;
   });
   
   await chrome.storage.local.get(["extraDetailedDescription"]).then((result) => {
-    EXTRA_DETAILED_DESCRIPTION = result.apiKey;
+    EXTRA_DETAILED_DESCRIPTION = result.extraDetailedDescription;
   });
 
   $(OVERWRITE_ALT_TAGS ? 'img' : 'img:not([alt]), img[alt=""]').each(async function () {
